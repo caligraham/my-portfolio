e256b12b8fd724f64f1ae51e3a363080fbe06423
Rebecca <graham.cali@gmail.com>
2021-04-30 15:26:26 -0500

    fixed my nav bar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import SinglePost from "./components/SinglePost";
@@ -9,15 +9,16 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <BrowserRouter>
-    <NavBar>
+    <NavBar />
     <Switch>
-      <Route component={Home} path='/' />
+      <Route exact path='/'>
+        <Home />
+      </Route>
       <Route component={About} path='/about' />
       <Route component={SinglePost} path='/post/:slug' />
       <Route component={Post} path='/post' />
       <Route component={Project} path='/project' />
     </Switch>
-    </NavBar>
     </BrowserRouter>
   )
 }
