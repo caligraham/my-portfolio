b32cc57622c6919d9ceddd86d601fe0d5047bf2f
Rebecca <graham.cali@gmail.com>
2021-04-30 13:54:53 -0500

    added icons and links to navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import {BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./components/Home"
-import About from "./components/About"
-import SinglePost from "./components/SinglePost"
-import Post from "./components/Post"
-import Project from "./components/Project"
+import Home from "./components/Home";
+import About from "./components/About";
+import SinglePost from "./components/SinglePost";
+import Post from "./components/Post";
+import Project from "./components/Project";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar>
     <Switch>
       <Route component={Home} path='/' />
       <Route component={About} path='/about' />
@@ -15,6 +17,7 @@ function App() {
       <Route component={Post} path='/post' />
       <Route component={Project} path='/project' />
     </Switch>
+    </NavBar>
     </BrowserRouter>
   )
 }
