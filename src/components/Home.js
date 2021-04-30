e3847162d12253754b4018a877df06a145e3c3d0
Rebecca <graham.cali@gmail.com>
2021-04-30 14:13:08 -0500

    css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import React from "react"
+import image from "../background.jpg"
 
 export default function Home(){
-    return <h1>Home Page!</h1>
-}
+    return (
+    <main>
+        <img src={image} alt="Coding Background" className="absolute object -cover w-full h-full"/>
+        <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+            <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hello, I'm Cali.</h1>
+        </section>
+    </main>
+)
+  }
