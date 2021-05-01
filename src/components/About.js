8cea0825d30301b2ce3784bebc69e8b3472b20e1
Rebecca <graham.cali@gmail.com>
2021-04-30 19:40:26 -0500

    started about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,33 @@
-import React from "react"
+import React, {useEffect, useState } from "react";
+import sanityClient from "../client.js";
+import aboutMe from "../aboutme.jpg"
 
 export default function About(){
-    return <h1>About Page!</h1>
+    const [author, setAuthor] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "author"]{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`).then((data) => setAuthor(data[0]))
+        .catch(console.error);
+    }, []);
+    if (!author) return <div>Loading....</div>;
+    return (
+        <main>
+            <img />
+            <div>
+                <section>
+                    <img />
+                    <div>
+                        <h1></h1>
+                        <div>
+                            BLOCK CONTENT
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
 }
