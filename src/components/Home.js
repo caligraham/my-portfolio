ed643bef858f44329466a6c8d4688141c8e23ae3
Rebecca <graham.cali@gmail.com>
2021-05-06 14:55:36 -0500

    alt for img

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ export default function Home(){
     <main>
         <img src={image} alt="Coding Background" className="absolute object -cover w-full h-full"/>
         <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">  
-            <img src={logo} className="logo"/>
+            <img src={logo} alt="" className="logo"/>
         </section>
     </main>
 )
