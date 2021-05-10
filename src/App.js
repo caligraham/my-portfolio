f21e1261bcfc7c2240113524e4fad6b57a684132
Rebecca <graham.cali@gmail.com>
2021-05-10 15:28:06 -0500

    removed unused component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
 import Project from "./components/Project";
 import NavBar from "./components/NavBar";
@@ -15,7 +14,6 @@ function App() {
         <Home />
       </Route>
       <Route component={About} path='/about' />
-      <Route component={SinglePost} path='/post/:slug' />
       <Route component={Post} path='/post' />
       <Route component={Project} path='/project' />
     </Switch>
